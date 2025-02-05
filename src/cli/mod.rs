e1c361842eb25fef4175b4c78dc6635b6ba17853
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-05 21:47:40 +0200

    `.first()` instead of `.get(0)`

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -79,7 +79,7 @@ pub fn start_app(args: Args) {
 
     for update in update_info {
         let title = {
-            if let Some(title) = update.titles.get(0) {
+            if let Some(title) = update.titles.first() {
                 title.clone()
             }
             else {
